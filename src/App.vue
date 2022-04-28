8bee2284bd04025653c432ca1896aa5a1e7ea9b5
kanon <kanon@ssd-tech.io>
2022-04-28 17:52:18 +0600

    import component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
-<div>
-<h1>{{name}}</h1>
-<h2>{{age}}</h2>
-</div>
-  
+  <div>
+  <app-head></app-head>
+    <h1>{{ name }}</h1>
+    <h2>{{ age }}</h2>
+    <app-foot></app-foot>
+  </div>
 </template>
 <script>
+  import VueProjectHeader from './Components/Header_Footer/header.vue';
+  import VueProjectFooter from './Components/Header_Footer/footer.vue';
 export default {
+
   data() {
     return {
-      name: 'kanon',
-      age:29
+      name: "kanon",
+      age: 29,
     };
   },
-}
+  components:{
+    'app-head':VueProjectHeader,
+    'app-foot':VueProjectFooter
+
+  }
+};
 </script>
 
 <style  scoped>
-  
 </style>
---
+++ src/Components/Header_Footer/footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>footer</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VueProjectFooter',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
