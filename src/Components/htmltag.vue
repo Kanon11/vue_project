7c8647d4294bad373e27494e14578d8045c16498
kanon <kanon@ssd-tech.io>
2022-05-10 20:31:21 +0600

    dynamic class

--- src/Components/htmltag.vue
+++ src/Components/htmltag.vue
@@ -1,13 +1,36 @@
 <template>
-    <div v-html="tag"></div>
+  <h1 v-html="tag" :class="change()"></h1>
+  <div :class="change()">kanon</div>
+  <button v-on:click="truFalse()">change</button>
 </template>
 <script>
-export default{
-    name:"htmlTag",
-    data(){
-        return{
-            tag:"<span>kanon</span>"
-        }
-    }
-}
+export default {
+  name: "htmlTag",
+  data() {
+    return {
+      tag: "<span>kanon</span>",
+      colorfull: true,
+    };
+  },
+  methods: {
+    truFalse() {
+      this.colorfull = !this.colorfull;
+    },
+    change() {
+      return {
+        green: this.colorfull,
+        red: true,
+      };
+    },
+  },
+};
 </script>
+
+<style scoped>
+.green {
+  background-color: blueviolet;
+}
+.red {
+  color: rgb(22, 151, 80);
+}
+</style>
